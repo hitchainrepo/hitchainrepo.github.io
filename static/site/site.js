f0fc34380d5076734e29b70d800d15c7e9e86302
tylerchen <iffiff1@gmail.com>
2020-07-31 10:07:00 +0800

    update: add 1.0.2 docs

--- static/site/site.js
+++ static/site/site.js
@@ -78,6 +78,7 @@ function loadTemplate($el, href, updateNav){
 }
 
 var hit_versions = [
+    {version: "1.0.2", url: "docs/1.0.2/index.html"},
     {version: "0.0.5", url: "docs/0.0.5/index.html"},
     {version: "0.0.4", url: "docs/0.0.4/index.html"},
     {version: "0.0.3", url: "docs/0.0.3/index.html"},
